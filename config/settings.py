5fcad75ca95a5a6e8a712111784009321bcb62e4
Kateleen Issa <kateleenissa@Jimis-MacBook-Air-2.local>
2026-06-29 09:35:29 -0400

    Move Celery broker config to environment variables

--- config/settings.py
+++ config/settings.py
@@ -137,5 +137,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL')
+CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND')
